8f79c25e09198dafec5f0f966e23773b4f5feef0
Zhu550400438 <33684664+Zhu550400438@users.noreply.github.com>
2019-09-05 20:26:33 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,95 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    包裹入库
+    <br /><br /><br />
+  <a-form
+    :form="form"
+    @submit="handleSubmit"
+  >
+    <a-form-item
+      label="运单号："
+      :label-col="{ span: 8}"
+      :wrapper-col="{ span: 11 }"
+    >
+      <a-input
+        v-decorator="[
+          '运单号：',
+          {rules: [{ required: true, message: 'Please input your note!' }]}
+        ]"
+      />
+    </a-form-item>
+     <a-form-item
+      label="收件人："
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 11 }"
+    >
+      <a-input
+        v-decorator="[
+          '收件人：',
+          {rules: [{ required: true, message: 'Please input your note!' }]}
+        ]"
+      />
+    </a-form-item> <a-form-item
+      label="电话： "
+      :label-col="{ span:8 }"
+      :wrapper-col="{ span: 11 }"
+    >
+      <a-input
+        v-decorator="[
+          '电话：',
+          {rules: [{ required: true, message: 'Please input your note!' }]}
+        ]"
+      />
+    </a-form-item> <a-form-item
+      label="重量： "
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 11 }"
+    >
+      <a-input
+        v-decorator="[
+          '重量：',
+          {rules: [{ required: true, message: 'Please input your note!' }]}
+        ]"
+      />
+    </a-form-item>
+  </a-form>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  data () {
+    return {
+      formLayout: 'horizontal',
+      form: this.$form.createForm(this),
+    };
+  },
+  methods: {
+    handleSubmit (e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    },
+    handleSelectChange (value) {
+      console.log(value);
+      this.form.setFieldsValue({
+        note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+      });
+    },
+  },
+};
+</script>
+
+<style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
