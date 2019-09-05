4e2530ad581bfda8bef2bfbf11e254ff2c9c77f3
Zhu550400438 <33684664+Zhu550400438@users.noreply.github.com>
2019-09-05 20:27:04 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import antd from 'ant-design-vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.use(Antd)
 
 Vue.config.productionTip = false
+Vue.use(antd)
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  render: h => h(App),
 }).$mount('#app')
